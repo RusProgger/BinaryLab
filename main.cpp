eef1849c96c52b40264396c4ac919d23e4e4bdad
RusProgger <ruslantimka92@gmail.com>
2026-04-19 07:40:24 +0300

    Add mainMenu

--- main.cpp
+++ main.cpp
@@ -9,7 +9,25 @@ void localeRus() {
 	SetConsoleCP(CP_UTF8);
 }
 
+// mainMenu 
 
+void mainMenu()
+{
+    std::cout << "====================================\n";
+    std::cout << "            BinaryLab               \n";
+    std::cout << "====================================\n\n";
+
+    std::cout << "BinaryLab is a simple tool for working\n";
+    std::cout << "with binary representations of numbers.\n\n";
+
+    std::cout << "Select language:\n\n";
+
+    std::cout << "  1) English\n";
+    std::cout << "  2) Russian\n";
+    std::cout << "  0) Exit\n\n";
+
+    std::cout << "Your choice: ";
+}
 
 
 
@@ -17,7 +35,8 @@ void localeRus() {
 
 int main() {
 
-    
+    // mainMenu
+    mainMenu();
     
 
 
