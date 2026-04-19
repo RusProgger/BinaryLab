687ece53b91d3760df15776c4ff014be7f8aa7fa
RusProgger <ruslantimka92@gmail.com>
2026-04-19 07:28:40 +0300

    Add locale Rus

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 #include <iomanip>
+#include <windows.h>
+
+// locale
+
+void localeRus() {
+    SetConsoleOutputCP(CP_UTF8);
+	SetConsoleCP(CP_UTF8);
+}
+
+
+
+
+
+
 
 int main() {
 
-
-
     
+    
+
+
     // clear buffer
     std::cin.ignore();
     // Pause
