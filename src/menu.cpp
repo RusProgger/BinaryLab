aacb38726f3ef81332f169527f2a7fd58609125d
RusProgger <ruslantimka92@gmail.com>
2026-06-08 23:49:39 +0300

    Add file menu.cpp

---
+++ src/menu.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "menu.h"
+
+void mainMenu()
+{
+    std::cout << "====================================\n";
+    std::cout << "            BinaryLab               \n";
+    std::cout << "====================================\n\n";
+
+    std::cout << "BinaryLab is a simple tool for working\n";
+    std::cout << "with binary representations of numbers.\n\n";
+
+    std::cout << "Select language:\n\n";
+
+    std::cout << "  1) English\n";
+    std::cout << "  2) Russian\n";
+    std::cout << "  0) Exit\n\n";
+}
