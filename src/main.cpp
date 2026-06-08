6c5e611685ba5f9db14b2ca589e77dea7ce083de
RusProgger <ruslantimka92@gmail.com>
2026-06-08 23:42:44 +0300

    add main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+#include <iomanip>
+#include <windows.h>
+
+// locale
+
+void localeRus() {
+    SetConsoleOutputCP(CP_UTF8);
+	SetConsoleCP(CP_UTF8);
+}
+
+// mainMenu 
+
+void mainMenu()
+{
+    std::cout << "====================================\n";
+    std::cout << "            BinaryLab               \n";
+    std::cout << "====================================\n\n";
+
+    std::cout << "BinaryLab is a simple tool for working\n";
+    std::cout << "with binary representations of numbers.\n\n";
+
+    std::cout << "Select language:\n\n";
+
+    std::cout << "  1) English\n";
+    std::cout << "  2) Russian\n";
+    std::cout << "  0) Exit\n\n";
+}
+
+// BinaryLab 
+
+// void printBinary(int n) {
+
+//     std::cout << "Number " << n  << ": ";
+
+//     for (int i = 31; i >= 0; i--) {
+//         std::cout << ((n >> i) & 1);
+//     }
+   
+//     std::cout << std::endl;
+// }
+
+
+void printBinary(int n) {
+
+    std::cout << "Number " << n << ": ";
+
+    int start = 31;
+
+    
+    while (start > 0 && ((n >> start) & 1) == 0) {
+        start--;
+    }
+
+    // min 7 bit
+    if (start < 6) {
+        start = 6;
+    }
+
+    for (int i = start; i >= 0; i--) {
+        std::cout << ((n >> i) & 1);
+    }
+
+    std::cout << std::endl;
+}
+
+
+int main() {
+
+    // mainMenu
+    mainMenu();
+
+    int number;
+
+    bool running = true;
+
+    int num {0};
+
+    // choice
+
+    while(running) {
+        std::cout << "Your choice: ";
+
+        if (!(std::cin >> number)) {
+
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+
+        std::cout << "Invalid input! Try again.\n";
+        continue;
+    }
+
+        // switch
+
+        switch(number) {
+            case 1: {
+            std::cout << "Enter number (or -1 to back): ";
+
+            while (true)
+            {
+                if (!(std::cin >> num))
+                {
+                    std::cin.clear();
+                    std::cin.ignore(10000, '\n');
+                    std::cout << "Invalid input! Try again: ";
+                    continue;
+                }
+
+                if (num == -1)
+                    break;
+
+                printBinary(num);
+                std::cout << "Enter number (or -1 to back): ";
+            }
+            break;
+        }
+
+                case 2: {
+                localeRus();
+
+                std::cout << "Введите число (или -1 для выхода): ";
+
+            while (true)
+            {
+                if (!(std::cin >> num))
+                {
+                    std::cin.clear();
+                    std::cin.ignore(10000, '\n');
+                    std::cout << "Неверный ввод! Попробуйте снова: ";
+                    continue;
+                }
+
+                if (num == -1)
+                    break;
+
+                printBinary(num);
+                std::cout << "Введите число (или -1 для выхода): ";
+            }
+            break;
+        }
+            case 0:
+                std::cout << "GoodBye...\n";
+                running = false;
+                break;
+            default:
+                std::cout << "Unknown option! Try 1, 2 or 0.\n";
+        } 
+        
+
+        //std::cout << "Unknown option! Try 1, 2 or 0.\n";
+
+    } // while end
+
+    
+    // Press any key to exit...
+    std::cout << "Press any key to exit...";
+
+    // clear buffer
+    std::cin.ignore();
+    // Pause
+    std::cin.get();
+    return 0;
+}
