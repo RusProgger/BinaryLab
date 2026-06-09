4d05e0359f916a2f9e9fc2a3d27ecdad28fe065a
RusProgger <ruslantimka92@gmail.com>
2026-06-09 22:51:22 +0300

    Add function printBinaryRus

--- src/binary.cpp
+++ src/binary.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 #include "binary.h"
 
-void printBinaryEng(int n)
-{
+void printBinaryEng(int n) {
     std::cout << "Number " << n << ": ";
+
+    int start = 31;
+
+    while (start > 0 && ((n >> start) & 1) == 0)
+    {
+        start--;
+    }
+
+    if (start < 6)
+    {
+        start = 6;
+    }
+
+    for (int i = start; i >= 0; i--)
+    {
+        std::cout << ((n >> i) & 1);
+    }
+
+    std::cout << std::endl;
+}
+
+void printBinaryRus(int n) {
+    std::cout << "Число: " << n << ": ";
 
     int start = 31;
 
